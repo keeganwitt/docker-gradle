d00ce3ad7632bc920e5dc2cb05439f61ce87039d
Keegan Witt <keeganwitt@gmail.com>
2016-12-17 16:50:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# gradle-docker
+This is the Git repo that will eventually (hopefully) become the official Docker images for Gradle.
+
+## Instructions for use
+For now, you'll have to build the images first until they're published on Docker Hub.
+
+### Building a Gradle project
+Run this from the directory of the Gradle project you want to build.
+
+`docker run -it --rm -v "$PWD":/project -w /project --name gradle gradle/jdk8-latest-alpine gradle <gradle-task>`
+
+## Instructions for a new Gradle release
+1. Change `ENV GRADLE_VERSION` in all Dockerfiles to new version number.
+1. Change `gradleVersion` (and `gradleMajorVersion` if applicable) variable(s) in _buildImages.sh_ to new version number(s).
+1. Download the binary zip.
+1. Run `sha256sum` on the above zip and change the `ENV GRADLE_DOWNLOAD_SHA256` in all Dockerfiles to new sha.
+1. Run _buildImages.sh_.
+
+### Prerequisites
+* Docker
+* sha256sum
---
+++ buildImages.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+gradleMajorVersion=3.2
+gradleVersion=${gradleMajorVersion}.1
+
+cd jre7
+docker build -t gradle:jre7-${gradleMajorVersion} -t gradle:jre7-${gradleVersion} -t gradle:jre7-latest .
+cd ../jdk7
+docker build -t gradle:jdk7-${gradleMajorVersion} -t gradle:jdk7-${gradleVersion} -t gradle:jdk7-latest .
+cd ../jre7-alpine
+docker build -t gradle:jre7-${gradleMajorVersion}-alpine -t gradle:jre7-${gradleVersion}-alpine -t gradle:jre7-latest-alpine .
+cd ../jdk7-alpine
+docker build -t gradle:jdk7-${gradleMajorVersion}-alpine -t gradle:jdk7-${gradleVersion}-alpine -t gradle:jdk7-latest-alpine .
+
+cd ../jre8
+docker build -t gradle:jre8-${gradleMajorVersion} -t gradle:jre8-${gradleVersion} -t gradle:jre8-latest .
+cd ../jdk8
+docker build -t gradle:jdk8-${gradleMajorVersion} -t gradle:jdk8-${gradleVersion} -t gradle:jdk8-latest .
+cd ../jre8-alpine
+docker build -t gradle:jre8-${gradleMajorVersion}-alpine -t gradle:jre8-${gradleVersion}-alpine -t gradle:jre8-latest-alpine .
+cd ../jdk8-alpine
+docker build -t gradle:jdk8-${gradleMajorVersion}-alpine -t gradle:jdk8-${gradleVersion}-alpine -t gradle:jdk8-latest-alpine .
+
+cd ..
