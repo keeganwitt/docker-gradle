7a21dec3b7e98071c313411fc7da96cd78722bd5
Keegan Witt <keeganwitt@gmail.com>
2017-01-31 03:11:07 -0500

    implemented recommendations by Docker team

--- test/run.sh
+++ test/run.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eu
 
-image=${1}
-expectedGradleVersion=${2}
+image=$1
+expectedGradleVersion=$2
 
-version=`docker run --rm "${image}" gradle --version | grep -E "Gradle [0-9.]+" | grep -Eo "[0-9.]+"`
+version=$(docker run --rm "${image}" gradle --version | grep -E "Gradle [0-9.]+" | grep -Eo "[0-9.]+")
 if [ "${version}" != "${expectedGradleVersion}" ]; then
     echo "version '${version}' does not match expected version '${expectedGradleVersion}'"
     exit 1
 fi
 
-if [ `echo "${image}" | grep 'jre'` == "" ]; then
-    if [ "`docker run --rm -v "${PWD}/java-quickstart:/project" -w /project "${image}" gradle clean test | grep 'BUILD SUCCESSFUL'`" == "" ]; then
+if [ $(echo "${image}" | grep 'jre') = "" ]; then
+    if [ "$(docker run --rm -v "${PWD}/java-quickstart:/project" -w "/project" "${image}" gradle clean test | grep 'BUILD SUCCESSFUL')" = "" ]; then
         echo "java-quickstart test failed"
         exit 1
     fi
