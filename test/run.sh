706f15127ed72ee0c417c09104cb8d8ef83e62c4
Keegan Witt <keeganwitt@gmail.com>
2017-08-09 21:42:10 -0400

    fixed silent test failure

--- test/run.sh
+++ test/run.sh
@@ -5,13 +5,13 @@ image=$1
 expectedGradleVersion=$2
 
 version=$(docker run --rm "${image}" gradle --version | grep  --extended-regexp "Gradle [0-9.]+" | grep  --extended-regexp --only-matching "[0-9.]+")
-if [ "${version}" != "${expectedGradleVersion}" ]; then
+if [[ "${version}" != "${expectedGradleVersion}" ]]; then
     echo "version '${version}' does not match expected version '${expectedGradleVersion}'"
     exit 1
 fi
 
-if [ $(echo "${image}" | grep 'jre') = "" ]; then
-    if [ "$(docker run --rm --volume "${PWD}/java-quickstart:/project" --workdir "/project" "${image}" gradle clean test | grep 'BUILD SUCCESSFUL')" = "" ]; then
+if [[ $(echo "${image}" | grep "jre") == "" ]]; then
+    if [[ $(docker run --rm --volume "${PWD}/java-quickstart:/project" --workdir "/project" "${image}" gradle clean test | grep "BUILD SUCCESSFUL") == "" ]]; then
         echo "java-quickstart test failed"
         exit 1
     fi
