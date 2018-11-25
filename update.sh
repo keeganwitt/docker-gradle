582f5e483e9657b09a0f47c7982a80a72369290d
Keegan Witt <keeganwitt@gmail.com>
2018-11-25 10:26:59 -0500

    make curl silent

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ set -o errexit -o nounset
 gradleVersion=${1}
 
 sed --regexp-extended --in-place "s/ENV GRADLE_VERSION .+/ENV GRADLE_VERSION ${gradleVersion}/" */Dockerfile
-sha=$(curl https://downloads.gradle.org/distributions/gradle-${gradleVersion}-bin.zip.sha256)
+sha=$(curl --fail --show-error --silent --location https://downloads.gradle.org/distributions/gradle-${gradleVersion}-bin.zip.sha256)
 sed --regexp-extended --in-place "s/GRADLE_DOWNLOAD_SHA256=.+$/GRADLE_DOWNLOAD_SHA256=${sha}/" */Dockerfile
 sed --regexp-extended --in-place "s/run.sh \"\\$\{image\}\" \".+\"/run.sh \"\\$\{image\}\" \"${gradleVersion}\"/" .travis.yml
