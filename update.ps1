306ed9b3955f8ea8e8468ce487d2268007e504d1
Keegan Witt <keeganwitt@gmail.com>
2018-11-25 11:07:26 -0500

    added PowerShell update script

---
+++ update.ps1
@@ -0,0 +1,8 @@
+$gradleVersion = $($args[0])
+$sha = $(Invoke-RestMethod -Uri https://downloads.gradle.org/distributions/gradle-${gradleVersion}-bin.zip.sha256)
+
+dir -Recurse -Filter Dockerfile | ForEach-Object {
+    (Get-Content -Path $_.FullName) -replace "ENV GRADLE_VERSION .+", "ENV GRADLE_VERSION ${gradleVersion}" | Set-Content $_.FullName
+    (Get-Content -Path $_.FullName) -replace "GRADLE_DOWNLOAD_SHA256=.+$", "GRADLE_DOWNLOAD_SHA256=${sha}" | Set-Content $_.FullName
+}
+(Get-Content -Path .travis.yml) -replace "run.sh `"\$\{image\}`" `".+`"", "run.sh `"`${image}`" `"${gradleVersion}`"" | Set-Content .travis.yml
