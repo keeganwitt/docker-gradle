f7d1450c6700feebc2e3b3750219306c4f035a36
Keegan Witt <keeganwitt@gmail.com>
2021-10-31 02:41:20 -0400

    Revert "Remove unneeded --location arg"
    This reverts commit 049625a0

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
-gradleVersion=$(curl --fail --show-error --silent https://services.gradle.org/versions/current | jq --raw-output .version)
-sha=$(curl --fail --show-error --silent "https://downloads.gradle.org/distributions/gradle-${gradleVersion}-bin.zip.sha256")
+gradleVersion=$(curl --fail --show-error --silent --location https://services.gradle.org/versions/current | jq --raw-output .version)
+sha=$(curl --fail --show-error --silent --location "https://downloads.gradle.org/distributions/gradle-${gradleVersion}-bin.zip.sha256")
 
 sed --regexp-extended --in-place "s/ENV GRADLE_VERSION .+$/ENV GRADLE_VERSION ${gradleVersion}/" ./*/Dockerfile
 sed --regexp-extended --in-place "s/GRADLE_DOWNLOAD_SHA256=.+$/GRADLE_DOWNLOAD_SHA256=${sha}/" ./*/Dockerfile
