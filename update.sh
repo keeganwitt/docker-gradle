d12897ab5299dcb6a7de6a8ab817ee22ad61191a
Keegan Witt <keeganwitt@gmail.com>
2021-10-31 02:04:29 -0400

    Quote variable expansion

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 set -o errexit -o nounset -o pipefail
 
 gradleVersion=$(curl --fail --show-error --silent https://services.gradle.org/versions/current | jq --raw-output .version)
-sha=$(curl --fail --show-error --silent https://downloads.gradle.org/distributions/gradle-${gradleVersion}-bin.zip.sha256)
+sha=$(curl --fail --show-error --silent "https://downloads.gradle.org/distributions/gradle-${gradleVersion}-bin.zip.sha256")
 
 sed --regexp-extended --in-place "s/ENV GRADLE_VERSION .+$/ENV GRADLE_VERSION ${gradleVersion}/" ./*/Dockerfile
 sed --regexp-extended --in-place "s/GRADLE_DOWNLOAD_SHA256=.+$/GRADLE_DOWNLOAD_SHA256=${sha}/" ./*/Dockerfile
