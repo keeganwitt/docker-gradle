4c08b642c05899abc968f0ce9b57f23ffcb1eeeb
Keegan Witt <keeganwitt@gmail.com>
2021-10-31 01:25:49 -0400

    Fix Dockerfile path

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ set -o errexit -o nounset -o pipefail
 gradleVersion=$(curl --fail --show-error --silent --location https://services.gradle.org/versions/current | jq --raw-output .version)
 sha=$(curl --fail --show-error --silent --location https://downloads.gradle.org/distributions/gradle-${gradleVersion}-bin.zip.sha256)
 
-sed --regexp-extended --in-place "s/ENV GRADLE_VERSION .+$/ENV GRADLE_VERSION ${gradleVersion}/" ./*/*/Dockerfile
-sed --regexp-extended --in-place "s/GRADLE_DOWNLOAD_SHA256=.+$/GRADLE_DOWNLOAD_SHA256=${sha}/" ./*/*/Dockerfile
+sed --regexp-extended --in-place "s/ENV GRADLE_VERSION .+$/ENV GRADLE_VERSION ${gradleVersion}/" ./*/Dockerfile
+sed --regexp-extended --in-place "s/GRADLE_DOWNLOAD_SHA256=.+$/GRADLE_DOWNLOAD_SHA256=${sha}/" ./*/Dockerfile
 sed --regexp-extended --in-place "s/expectedGradleVersion: .+$/expectedGradleVersion: ${gradleVersion}/" .github/workflows/ci.yaml
