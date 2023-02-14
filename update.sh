0aa9d9ff005c27e0b10de36e91dab65f4a6dfabd
Clayton Walker <clayton.m.walker@gmail.com>
2023-02-14 14:31:06 -0700

    Upgrade to gradle 8.0

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ sha=$(curl --fail --show-error --silent --location "https://downloads.gradle.org
 
 sed --regexp-extended --in-place "s/ENV GRADLE_VERSION .+$/ENV GRADLE_VERSION ${gradleVersion}/" ./*/Dockerfile
 sed --regexp-extended --in-place "s/GRADLE_DOWNLOAD_SHA256=.+$/GRADLE_DOWNLOAD_SHA256=${sha}/" ./*/Dockerfile
-sed --regexp-extended --in-place "s/expectedGradleVersion: .+$/expectedGradleVersion: ${gradleVersion}/" .github/workflows/ci.yaml
+sed --regexp-extended --in-place "s/expectedGradleVersion: .+$/expectedGradleVersion: \"${gradleVersion}\"/" .github/workflows/ci.yaml
