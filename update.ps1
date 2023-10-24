861ddd1043417ca31622b7a7084f1f9bcb75b1ad
Keegan Witt <keeganwitt@gmail.com>
2023-10-24 12:19:08 -0400

    Fix that latest Graal release might not be in the first 2

--- update.ps1
+++ update.ps1
@@ -1,8 +1,12 @@
 $gradleVersion = $((Invoke-WebRequest "https://services.gradle.org/versions/current" | ConvertFrom-Json).version)
 $sha = $(Invoke-RestMethod -Uri "https://downloads.gradle.org/distributions/gradle-${gradleVersion}-bin.zip.sha256")
 
-$latestGraal17 = $(((Invoke-WebRequest "https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=4&page=1" | ConvertFrom-Json).tag_name | Select-String -Pattern "jdk-17").ToString().Replace("jdk-", ""))
-$latestGraal21 = $(((Invoke-WebRequest "https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=4&page=1" | ConvertFrom-Json).tag_name | Select-String -Pattern "jdk-21").ToString().Replace("jdk-", ""))
+$latestGraal17 = $(((Invoke-WebRequest "https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1" | ConvertFrom-Json).tag_name | Select-String -Pattern "jdk-17" | Select-Object -First 1).ToString().Replace("jdk-", ""))
+$latestGraal21 = $(((Invoke-WebRequest "https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1" | ConvertFrom-Json).tag_name | Select-String -Pattern "jdk-21" | Select-Object -First 1).ToString().Replace("jdk-", ""))
+
+Write-Host "Latest Gradle version is $gradleVersion"
+Write-Host "Latest Graal 17 version is $latestGraal17"
+Write-Host "Latest Graal 21 version is $latestGraal21"
 
 dir -Recurse -Filter Dockerfile | ForEach-Object {
     (Get-Content -Path $_.FullName) -replace "ENV GRADLE_VERSION .+$", "ENV GRADLE_VERSION ${gradleVersion}" | Set-Content $_.FullName
--- update.sh
+++ update.sh
@@ -8,8 +8,12 @@ sed --regexp-extended --in-place "s/ENV GRADLE_VERSION .+$/ENV GRADLE_VERSION ${
 sed --regexp-extended --in-place "s/GRADLE_DOWNLOAD_SHA256=.+$/GRADLE_DOWNLOAD_SHA256=${sha}/" ./*/Dockerfile
 sed --regexp-extended --in-place "s/expectedGradleVersion: .+$/expectedGradleVersion: \"${gradleVersion}\"/" .github/workflows/ci.yaml
 
-latestGraal17=$(curl --silent --location 'https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=2&page=1' | jq -r 'map(select(.tag_name | contains("jdk-17"))) | .[0].tag_name | sub("jdk-"; "")')
+latestGraal17=$(curl --silent --location 'https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1' | jq -r 'map(select(.tag_name | contains("jdk-17"))) | .[0].tag_name | sub("jdk-"; "")')
 sed --regexp-extended --in-place "s/JDK_VERSION=[^ ]+/JDK_VERSION=${latestGraal17}/" ./jdk17*graal/Dockerfile
 
-latestGraal21=$( curl --silent --location 'https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=2&page=1' | jq -r 'map(select(.tag_name | contains("jdk-21"))) | .[0].tag_name | sub("jdk-"; "")')
+latestGraal21=$( curl --silent --location 'https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1' | jq -r 'map(select(.tag_name | contains("jdk-21"))) | .[0].tag_name | sub("jdk-"; "")')
 sed --regexp-extended --in-place "s/JDK_VERSION=[^ ]+/JDK_VERSION=${latestGraal21}/" ./jdk21*graal/Dockerfile
+
+echo "Latest Gradle version is ${gradleVersion}"
+echo "Latest Graal 17 version is ${latestGraal17}"
+echo "Latest Graal 21 version is ${latestGraal21}"
