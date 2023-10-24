b26b7673ec3396db92a7ab563d6b439501f68c49
Keegan Witt <keeganwitt@gmail.com>
2023-10-24 13:27:44 -0400

    Update checksums in update scripts

--- update.ps1
+++ update.ps1
@@ -1,23 +1,28 @@
 $gradleVersion = $((Invoke-WebRequest "https://services.gradle.org/versions/current" | ConvertFrom-Json).version)
 $sha = $(Invoke-RestMethod -Uri "https://downloads.gradle.org/distributions/gradle-${gradleVersion}-bin.zip.sha256")
 
-$latestGraal17 = $(((Invoke-WebRequest "https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1" | ConvertFrom-Json).tag_name | Select-String -Pattern "jdk-17" | Select-Object -First 1).ToString().Replace("jdk-", ""))
-$latestGraal21 = $(((Invoke-WebRequest "https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1" | ConvertFrom-Json).tag_name | Select-String -Pattern "jdk-21" | Select-Object -First 1).ToString().Replace("jdk-", ""))
+$wc = [System.Net.WebClient]::new()
+$graal17Version = $(((Invoke-WebRequest "https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1" | ConvertFrom-Json).tag_name | Select-String -Pattern "jdk-17" | Select-Object -First 1).ToString().Replace("jdk-", ""))
+$graal17Hash = Get-FileHash -InputStream ($wc.OpenRead("https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${graal17Version}/graalvm-community-jdk-${graal17Version}_linux-x64_bin.tar.gz"))
+$graal21Version = $(((Invoke-WebRequest "https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1" | ConvertFrom-Json).tag_name | Select-String -Pattern "jdk-21" | Select-Object -First 1).ToString().Replace("jdk-", ""))
+$graal21Hash = Get-FileHash -InputStream ($wc.OpenRead("https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${graal21Version}/graalvm-community-jdk-${graal21Version}_linux-x64_bin.tar.gz"))
 
 Write-Host "Latest Gradle version is $gradleVersion"
-Write-Host "Latest Graal 17 version is $latestGraal17"
-Write-Host "Latest Graal 21 version is $latestGraal21"
+Write-Host "Latest Graal 17 version is $graal17Version"
+Write-Host "Latest Graal 21 version is $graal21Version"
 
 dir -Recurse -Filter Dockerfile | ForEach-Object {
     (Get-Content -Path $_.FullName) -replace "ENV GRADLE_VERSION .+$", "ENV GRADLE_VERSION ${gradleVersion}" | Set-Content $_.FullName
     (Get-Content -Path $_.FullName) -replace "GRADLE_DOWNLOAD_SHA256=.+$", "GRADLE_DOWNLOAD_SHA256=${sha}" | Set-Content $_.FullName
     if ($((Get-Item $_.FullName).Directory.Name) -match "jdk17.+graal")
     {
-        (Get-Content -Path $_.FullName) -replace "JAVA_VERSION=[^ ]+", "JAVA_VERSION=${latestGraal17}" | Set-Content $_.FullName
+        (Get-Content -Path $_.FullName) -replace "JAVA_VERSION=[^ ]+", "JAVA_VERSION=${graal17Version}" | Set-Content $_.FullName
+        (Get-Content -Path $_.FullName) -replace "GRAALVM_DOWNLOAD_SHA256=[^ ]+", "GRAALVM_DOWNLOAD_SHA256=${graal17Hash}" | Set-Content $_.FullName
     }
     if ($((Get-Item $_.FullName).Directory.Name) -match "jdk21.+graal")
     {
-        (Get-Content -Path $_.FullName) -replace "JAVA_VERSION=[^ ]+", "JAVA_VERSION=${latestGraal21}" | Set-Content $_.FullName
+        (Get-Content -Path $_.FullName) -replace "JAVA_VERSION=[^ ]+", "JAVA_VERSION=${graal21Version}" | Set-Content $_.FullName
+        (Get-Content -Path $_.FullName) -replace "GRAALVM_DOWNLOAD_SHA256=[^ ]+", "GRAALVM_DOWNLOAD_SHA256=${graal21Hash}" | Set-Content $_.FullName
     }
 }
 
--- update.sh
+++ update.sh
@@ -8,12 +8,16 @@ sed --regexp-extended --in-place "s/ENV GRADLE_VERSION .+$/ENV GRADLE_VERSION ${
 sed --regexp-extended --in-place "s/GRADLE_DOWNLOAD_SHA256=.+$/GRADLE_DOWNLOAD_SHA256=${sha}/" ./*/Dockerfile
 sed --regexp-extended --in-place "s/expectedGradleVersion: .+$/expectedGradleVersion: \"${gradleVersion}\"/" .github/workflows/ci.yaml
 
-latestGraal17=$(curl --silent --location 'https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1' | jq -r 'map(select(.tag_name | contains("jdk-17"))) | .[0].tag_name | sub("jdk-"; "")')
-sed --regexp-extended --in-place "s/JAVA_VERSION=[^ ]+/JAVA_VERSION=${latestGraal17}/" ./jdk17*graal/Dockerfile
+graal17Version=$(curl --silent --location 'https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1' | jq -r 'map(select(.tag_name | contains("jdk-17"))) | .[0].tag_name | sub("jdk-"; "")')
+sed --regexp-extended --in-place "s/JAVA_VERSION=[^ ]+/JAVA_VERSION=${graal17Version}/" ./jdk17*graal/Dockerfile
+graal17Sha=$(curl --fail --location --silent "https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${graal17Version}/graalvm-community-jdk-${graal17Version}_linux-x64_bin.tar.gz" | sha256sum | cut -d' ' -f1)
+sed --regexp-extended --in-place "s/GRAALVM_DOWNLOAD_SHA256=[^ ]+/GRAALVM_DOWNLOAD_SHA256=${graal17Sha}/" ./jdk17*graal/Dockerfile
 
-latestGraal21=$( curl --silent --location 'https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1' | jq -r 'map(select(.tag_name | contains("jdk-21"))) | .[0].tag_name | sub("jdk-"; "")')
-sed --regexp-extended --in-place "s/JAVA_VERSION=[^ ]+/JAVA_VERSION=${latestGraal21}/" ./jdk21*graal/Dockerfile
+graal21Version=$( curl --silent --location 'https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1' | jq -r 'map(select(.tag_name | contains("jdk-21"))) | .[0].tag_name | sub("jdk-"; "")')
+sed --regexp-extended --in-place "s/JAVA_VERSION=[^ ]+/JAVA_VERSION=${graal21Version}/" ./jdk21*graal/Dockerfile
+graal21Sha=$(curl --fail --location --silent "https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-${graal21Version}/graalvm-community-jdk-${graal21Version}_linux-x64_bin.tar.gz" | sha256sum | cut -d' ' -f1)
+sed --regexp-extended --in-place "s/GRAALVM_DOWNLOAD_SHA256=[^ ]+/GRAALVM_DOWNLOAD_SHA256=${graal21Sha}/" ./jdk21*graal/Dockerfile
 
 echo "Latest Gradle version is ${gradleVersion}"
-echo "Latest Graal 17 version is ${latestGraal17}"
-echo "Latest Graal 21 version is ${latestGraal21}"
+echo "Latest Graal 17 version is ${graal17Version}"
+echo "Latest Graal 21 version is ${graal21Version}"
