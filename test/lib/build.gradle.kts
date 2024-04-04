3c12b9a53fb8cb6c38d24a7706b39cae364344cb
Keegan Witt <keeganwitt@gmail.com>
2024-04-03 23:37:27 -0400

    Update test dependencies

--- test/lib/build.gradle.kts
+++ test/lib/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 }
 
 tasks.named<Test>("test") {
