c1feb140696047ef7b92dce3c1fce334528dc83e
Tianon Gravi <admwiggin@gmail.com>
2025-02-13 10:09:56 -0800

    Remove unmaintained Graal and (EOL) JDK 20 variants

--- update.sh
+++ update.sh
@@ -7,9 +7,3 @@ sha=$(curl --fail --show-error --silent --location "https://downloads.gradle.org
 sed --regexp-extended --in-place "s/ENV GRADLE_VERSION .+$/ENV GRADLE_VERSION ${gradleVersion}/" ./*/Dockerfile
 sed --regexp-extended --in-place "s/GRADLE_DOWNLOAD_SHA256=.+$/GRADLE_DOWNLOAD_SHA256=${sha}/" ./*/Dockerfile
 sed --regexp-extended --in-place "s/expectedGradleVersion: .+$/expectedGradleVersion: \"${gradleVersion}\"/" .github/workflows/ci.yaml
-
-latestGraal17=$(curl --silent --location 'https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1' | jq -r 'map(select(.tag_name | contains("jdk-17"))) | .[0].tag_name | sub("jdk-"; "")')
-sed --regexp-extended --in-place "s/JDK_VERSION=[^ ]+/JDK_VERSION=${latestGraal17}/" ./jdk17*graal/Dockerfile
-
-latestGraal20=$( curl --silent --location 'https://api.github.com/repos/graalvm/graalvm-ce-builds/releases?per_page=6&page=1' | jq -r 'map(select(.tag_name | contains("jdk-20"))) | .[0].tag_name | sub("jdk-"; "")')
-sed --regexp-extended --in-place "s/JDK_VERSION=[^ ]+/JDK_VERSION=${latestGraal20}/" ./jdk20*graal/Dockerfile
