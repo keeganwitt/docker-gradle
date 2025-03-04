774ac009567d0640091b8856705ad6a78b4468ae
Keegan Witt <keeganwitt@gmail.com>
2025-03-04 14:20:27 -0500

    Fix trailing whitespace in dir variable

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -106,6 +106,8 @@ for branch in "${branches[@]}"; do
 
 	firstVersion=
 	for dir in "${directories[@]}"; do
+	  # shellcheck disable=SC2001
+		dir="$(echo "$dir" | sed -e 's/[[:space:]]*$//')"
 		if [ ! -d "$dir" ]; then
 			# skip directory that doesn't exist in this branch
 			continue
